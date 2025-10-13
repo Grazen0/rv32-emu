e8c56810746c76ad0c9e70c543c98218604e42a6
José Daniel Grayson <josedanielgrayson@proton.me>
2025-10-12 23:54:13 -0500

    build: use riscv32-unknown-none-elf toolchain

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
         let
           riscvCross = import nixpkgs {
             inherit system;
-            crossSystem.config = "riscv32-none-elf";
+            crossSystem.config = "riscv32-unknown-none-elf";
           };
         in
         {
